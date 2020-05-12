af2b1eaabd44569af0bac169fedfec7afc86c2b2
Adaptrix <mwakalingajohn@gmail.com>
2020-05-12 18:19:21 +0300

    - implement to do service with add, fetch and delete - the method 1 complete

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from './todo';
+import { Observable } from 'rxjs';
+import { catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  rootUrl: string = "http://localhost:3000/todos"
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type':'application/json'
+    })
+  };
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>( `${this.rootUrl}`, this.httpOptions)
+  }
+
+  deleteTodo(id:string){
+    let url = `${this.rootUrl}/${id}`
+    console.log(url);
+
+    return this.http.delete(url,this.httpOptions)
+  }
+
+  addTodo( todo: Todo){
+    return this.http.post(`${this.rootUrl}`, todo, this.httpOptions)
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -12,16 +13,26 @@ export class TodosComponent implements OnInit {
 
   todos$: Observable<Todo[]>
 
-  constructor() { }
+  constructor(
+    private todosService: TodoService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchTodos()
   }
 
   onDeleteClickHandler(id:string):void{
-
+    this.todosService.deleteTodo(id)
+      .subscribe(r=>{ 
+        this.fetchTodos()
+      })
   }
 
   fetchTodos():void{
+    this.todos$ = this.todosService.getTodos()
+  }
 
+  onRefreshClickHandler(){
+    this.fetchTodos()
   }
 }
